7f75975b3c44aa4bffafde17ac639f3ffc590783
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2024-03-21 11:48:10 +0530

    getproductbyid and getallproduct 3rd party API integration

---
+++ src/main/java/com/example/productservice/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package com.example.productservice.controllers;
+
+import com.example.productservice.models.Product;
+import com.example.productservice.services.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //localhost:8080/products/30
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id) {
+        return productService.getProductById(id);
+    }
+
+    //localhost:8080/products
+    @GetMapping("/")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    //create a Product
+    @PostMapping("/")
+    public Product createProduct(@RequestBody Product product) {
+        return new Product();
+    }
+
+    //Partial Update.
+    @PatchMapping("/{id}")
+    public Product updateProduct(@PathVariable("id") Long id,@RequestBody Product product) {
+        return new Product();
+    }
+
+    //Replace Product
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable("id") Long id,@RequestBody Product product) {
+        return new Product();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+
+    }
+}
---
+++ src/main/java/com/example/productservice/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.productservice.services;
+
+import com.example.productservice.models.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    Product getProductById(Long id);
+
+    List<Product> getAllProducts();
+
+    Product updateProduct();
+
+    Product replaceProduct();
+
+    Product createProduct();
+
+    void deleteProduct();
+}
